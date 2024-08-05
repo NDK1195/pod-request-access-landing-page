96e47b45db49e1eb98b56fc7d63d0b93cf9c6f17
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-08-05 11:34:53 +0700

    add project structure

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#54E6AF",
+        "blue-950": "#121725",
+        "blue-900": "#2C344B",
+        "blue-600": "#5A668A",
+        "blue-300": "#C2CBE5",
+        red: "FB3E3E",
+      },
+    },
+  },
+  plugins: [],
+};
